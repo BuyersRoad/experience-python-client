46bc6b8d7e3d52a6e508e4e75c8f60c0efe8136b
GDanielM <gifton36@gmail.com>
2021-10-20 22:07:04 +0530

    monthly schedule

--- powerbi/pshell_monthly.ps1
+++ powerbi/pshell_monthly.ps1
@@ -4,5 +4,5 @@ $task_name = $args[1]
 $directory_location = $args[2]
 $powerbi_script_path = $args[3]
 $action = New-ScheduledTaskAction -Execute $script_type -WorkingDirectory $directory_location -Argument $powerbi_script_path
-$trigger = New-ScheduledTaskTrigger -Weekly -WeeksInterval 4 -DaysOfWeek Monday -At $schedule_time
+$trigger = New-ScheduledTaskTrigger -Monthly -At $schedule_time -DaysOfWeek Monday 
 Register-ScheduledTask $task_name -Action $action -Trigger $trigger
